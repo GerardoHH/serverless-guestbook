2d96a061730c31fa514cc555d069a2d52360c260
GerardoHH <gerardohernandez.hernandez@gmail.com>
2021-08-20 23:48:47 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://909b3a5b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
